56f7fcecd567f9172cfd38a73de6159bfd7e8dc4
Ramiz <79998135+ramizqurbanli@users.noreply.github.com>
2021-11-05 18:25:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //js
 if(navigator.userAgent.match(/firefox|fxios/i))
 {
- document.querySelector("html").innerHTML="<h1 style='color:red;'>Not Supported in Firefox browser</h1>";
+ document.querySelector("html").innerHTML="<h1 style='color:red;font-size:100px;text-align:center;'>Not Supported in Firefox browser</h1>";
 }
