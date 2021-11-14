26628c2218e5177999fe4c83ca176153ff1c3073
Quantum <turandeveloper@gmail.com>
2021-11-14 21:31:08 +0400

    yes
    changed

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ if(navigator.userAgent.match(/firefox|fxios/i))
 {
  document.querySelector("html").innerHTML="<h1 style='color:red;font-size:100px;text-align:center;'>Not Supported in Firefox browser</h1>";
 }
+function url(link){
+ window.open(link);
+}
