3bc6c87444c547f02c0643029342339a1a3135ba
Ramiz <79998135+ramizqurbanli@users.noreply.github.com>
2021-11-16 10:08:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //js
 if(navigator.userAgent.match(/firefox|fxios/i))
 {
- document.querySelector("htm").innerHTML="<h1 style='color:red;font-size:100px;text-align:center;'>Not Supported in Firefox browser</h1>";
+ document.querySelector("html").innerHTML="<h1 style='color:red;font-size:100px;text-align:center;'>Not Supported in Firefox browser</h1>";
 }
 function checklang(){
      console.log(document.querySelector(".lang").value);
