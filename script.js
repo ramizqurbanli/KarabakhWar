08e3154e7cfd15c3651aa932148784f8aa98e582
Ramiz <79998135+ramizqurbanli@users.noreply.github.com>
2021-12-05 18:50:55 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-//js
-if(navigator.userAgent.match(/firefox|fxios/i))
-{
- document.querySelector("html").innerHTML="<h1 style='color:red;font-size:100px;text-align:center;'>Not Supported in Firefox browser</h1>";
-}
+
 function checklang(){
      console.log(document.querySelector(".lang").value);
 }
